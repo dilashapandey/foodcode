64c12569f388c75c131f674492b1624146156fe8
Dilasha Pandey <pandeydilasha@gmail.com>
2025-04-05 18:53:33 +0545

    crud in restaurant

--- controllers/reataurantController.js
+++ controllers/reataurantController.js
@@ -30,4 +30,90 @@ const addRestaurant = async (req, res) => {
         });
     }
 }
-module.exports = { addRestaurant };
+
+const getAllRestaurant = async (req, res) => {
+    try {
+        const restaurant = await restaurantModel.find({});
+        if (!restaurant) {
+            return res.status(404).send({ message: "No restaurant found" });
+        }
+        res.status(200).send({
+            message: "Restaurants fetched successfully",
+            restaurant,
+        });
+    } catch (error) {
+        console.error("Error fetching all restaurants:", error);
+        res.status(500).send({
+            message: "Internal server error",
+        });        
+    }
+}
+
+const getRestaurant = async (req, res) => {
+    const restaurantId = req.params.id;
+    if (!restaurantId) {
+        return res.status(400).send({ message: "Restaurant ID is required" });
+    }
+    try {
+        const restaurant = await restaurantModel.findById(restaurantId);
+        if (!restaurant) {
+            return res.status(404).send({ message: "Restaurant not found" });
+        }
+        res.status(200).send({
+            message: "Restaurant fetched successfully",
+            restaurant,
+        });   
+    } catch (error) {
+        console.error("Error fetching restaurant:", error);
+        res.status(500).send({
+            message: "Internal server error",
+        });
+    }
+}
+
+const updateRestaurant = async (req, res) => {
+    const restaurantId = req.params.id;
+    if (!restaurantId) {
+        return res.status(400).send({ message: "Restaurant ID is required" });
+    }
+    try {
+        const restaurant = await restaurantModel.findByIdAndUpdate(restaurantId, req.body, { new: true });
+        if (!restaurant) {
+            return res.status(404).send({ message: "Restaurant not found" });
+        }
+        res.status(200).send({
+            message: "Restaurant updated successfully",
+            restaurant,
+        });
+    } catch (error) {
+        console.error("Error updating restaurant:", error);
+        res.status(500).send({
+            message: "Internal server error",
+        });
+    }
+}
+
+const deleteRestaurant =async (req, res) => {
+    const restaurantId = req.params.id;
+    if(!restaurantId){
+        return res.status(400).send({ message: "Restaurant ID is required" });
+    }
+    try {
+        const restaurant = await restaurantModel.findByIdAndDelete(restaurantId);
+        if (!restaurant) {
+            return res.status(404).send({ message: "Restaurant not found" });
+        }
+        res.status(200).send({
+            message: "Restaurant deleted successfully",
+            restaurant,
+        });
+    } catch (error) {
+        console.error("Error deleting restaurant:", error);
+        res.status(500).send({
+            message: "Internal server error",
+        });
+        
+    }
+}
+
+module.exports = { addRestaurant ,getAllRestaurant, getRestaurant, updateRestaurant, deleteRestaurant };
--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -1,11 +1,14 @@
 const restaurantModel = require('../models/restaurantModel');
 const express = require('express');
 const router = express.Router();
-const { getRestaurants, addRestaurant, updateRestaurant, deleteRestaurant } = require('../controllers/reataurantController');
+const { getAllRestaurant, getRestaurant, addRestaurant, updateRestaurant, deleteRestaurant } = require('../controllers/reataurantController');
 const isLoggedIn = require('../middleware/isloggedin'); 
 
 // Route to get all restaurants
 router.post('/create', isLoggedIn, addRestaurant);
+router.get('/all', getAllRestaurant);
+router.get('/:id', getRestaurant);
+router.put('/:id', isLoggedIn, updateRestaurant);
+router.delete('/:id', isLoggedIn, deleteRestaurant);
 
-
-module.exports = router;
+module.exports = router; 
